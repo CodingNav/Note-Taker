aa59274b26eadf5eb33c2f105987de88e57f6483
CodingNav <navleenkaurr@gmail.com>
2021-09-02 21:13:40 -0400

    Changed get route to *

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ app.use(express.static('public'));
 
 app.use('/api', api);
 
-// GET Route for home page
-app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 // GET Route for notes page
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for home page
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>

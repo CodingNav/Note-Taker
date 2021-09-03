f0cfe2f8032fccd4bbe2e29ee1cfeeec0d930a8b
CodingNav <navleenkaurr@gmail.com>
2021-09-02 21:03:08 -0400

    Set up delete request for the notes

--- routes/notes.js
+++ routes/notes.js
@@ -26,5 +26,15 @@ router.post('/', (req, res) => {
     });
 });
 
+router.delete('/:id', (req, res) => {
+    const index = req.params.id - 1;
+    fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
+        const parsedData = JSON.parse(data);
+        parsedData.splice(index, 1);
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(parsedData), (err) => {
+            res.json(parsedData);
+        });
+    });
+});
 
 module.exports = router;
